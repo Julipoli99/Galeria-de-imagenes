fd4cd866860f8c2ce1f290d6adc77410eb48fc57
Julipoli99 <Polimenijulian9@gmail.com>
2021-12-05 01:44:24 -0300

    .

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "proyectito-img",
-    "host": "127.0.0.1",
+    "username": "251559_juli",
+    "password": "julipoli99",
+    "database": "julianpolimeni_imagenes_proyecto",
+    "host": "julianpolimeni.alwaysdata.net",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "251559_juli",
+    "password": "julipoli99",
+    "database": "julianpolimeni_imagenes_proyecto",
+    "host": "julianpolimeni.alwaysdata.net",
     "dialect": "mysql"
   },
-  "production": {
+  /*"production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  }
+  }*/
 }
